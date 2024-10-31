e6894e32b46184b682bb8d58ea559eceee9da492
Christian Santiago Vera Rojas <christian.vera@globant.com>
2024-10-31 16:25:38 -0500

    Initial Commit

---
+++ ViewBindingPractice/ViewBindingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBindingPracticeApp.swift
+//  ViewBindingPractice
+//
+//  Created by Christian Santiago Vera Rojas on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewBindingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViewBindingPracticeTests/ViewBindingPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBindingPracticeTests.swift
+//  ViewBindingPracticeTests
+//
+//  Created by Christian Santiago Vera Rojas on 31/10/24.
+//
+
+import Testing
+@testable import ViewBindingPractice
+
+struct ViewBindingPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
